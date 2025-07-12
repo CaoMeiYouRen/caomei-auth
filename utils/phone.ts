c6817a686d30fac3167ca72a5fffee88fd0faad3
CaoMeiYouRen <caomeiyouren@qq.com>
2025-07-13 02:24:33 +0800

    feat(phone): 新增手机号区域代码自动获取功能
    - 新增getRegionCodeForPhoneNumber函数用于自动获取手机号区域代码
    - 修改formatPhoneNumber和formatPhoneNumberInternational函数参数默认值
    - 导出phoneUtil实例供外部使用

--- utils/phone.ts
+++ utils/phone.ts
@@ -1,6 +1,6 @@
-import { PhoneNumberFormat, PhoneNumberUtil, type RegionCode } from 'google-libphonenumber'
+import { PhoneNumberFormat, PhoneNumberUtil, type RegionCode, type RegionCodeUnknown } from 'google-libphonenumber'
 
-const phoneUtil = PhoneNumberUtil.getInstance()
+export const phoneUtil = PhoneNumberUtil.getInstance()
 
 /**
  * 格式化手机号到 E164 格式，用于数据库存储。
@@ -10,24 +10,44 @@ const phoneUtil = PhoneNumberUtil.getInstance()
  * @date 2025-07-12
  * @export
  * @param phoneNumber
- * @param region 手机号所属区域，默认中国大陆地区
+ * @param region 手机号所属区域。如果没有区域代码，则根据手机号自动获取，默认为中国大陆地区
  */
-export function formatPhoneNumber(phoneNumber: string, region: RegionCode = 'CN') {
+export function formatPhoneNumber(phoneNumber: string, region: RegionCode | RegionCodeUnknown = getRegionCodeForPhoneNumber(phoneNumber)) {
     const phoneNumberObj = phoneUtil.parse(phoneNumber, region)
     return phoneUtil.format(phoneNumberObj, PhoneNumberFormat.E164)
 }
 
 /**
- * 格式化手机号到国际格式，可读性更高。
+ * 格式化手机号到国际格式，用于展现给用户。
  * 例如 +1 202-456-1414
  *
  * @author CaoMeiYouRen
  * @date 2025-07-12
  * @export
  * @param phoneNumber
- * @param region 手机号所属区域，默认中国大陆地区
+ * @param region 手机号所属区域。如果没有区域代码，则根据手机号自动获取，默认为中国大陆地区
  */
-export function formatPhoneNumberInternational(phoneNumber: string, region: RegionCode = 'CN') {
+export function formatPhoneNumberInternational(phoneNumber: string, region: RegionCode | RegionCodeUnknown = getRegionCodeForPhoneNumber(phoneNumber)) {
     const phoneNumberObj = phoneUtil.parse(phoneNumber, region)
     return phoneUtil.format(phoneNumberObj, PhoneNumberFormat.INTERNATIONAL)
 }
+
+/**
+ * 从手机号中获取区域代码。
+ * 如果手机号中没有区域代码，则默认返回中国大陆地区代码。
+ *
+ * @author CaoMeiYouRen
+ * @date 2025-07-13
+ * @export
+ * @param phoneNumber
+ * @returns RegionCode
+ */
+export function getRegionCodeForPhoneNumber(phoneNumber: string): RegionCode | RegionCodeUnknown {
+    try {
+        const countryCode = phoneUtil.parse(phoneNumber).getCountryCodeOrDefault() || 86
+        return phoneUtil.getRegionCodeForCountryCode(countryCode)
+    } catch (error) {
+        return 'CN'
+    }
+}
+
