a3cbe3be5feddfbea58e76c83772043bff2c1781
CaoMeiYouRen <caomeiyouren@qq.com>
2025-07-25 22:42:20 +0800

    chore(database): 添加双因素认证表及相关索引，增强安全性

--- database/postgres/create.sql
+++ database/postgres/create.sql
@@ -1,45 +1,19 @@
--- public.caomei_auth_account definition
+-- public.caomei_auth_two_factor definition
 
 -- Drop table
 
--- DROP TABLE public.caomei_auth_account;
+-- DROP TABLE public.caomei_auth_two_factor;
 
-CREATE TABLE public.caomei_auth_account (
+CREATE TABLE public.caomei_auth_two_factor (
 	id varchar(36) NOT NULL,
 	created_at timestamptz DEFAULT now() NOT NULL,
 	updated_at timestamptz DEFAULT now() NOT NULL,
-	account_id text NOT NULL,
-	provider_id text NOT NULL,
 	user_id varchar(36) NOT NULL,
-	access_token text NULL,
-	refresh_token text NULL,
-	id_token text NULL,
-	access_token_expires_at timestamptz NULL,
-	refresh_token_expires_at timestamptz NULL,
-	"scope" text NULL,
-	"password" text NULL,
-	CONSTRAINT "PK_60bb887dbada52e154a3169c36e" PRIMARY KEY (id)
+	secret text NOT NULL,
+	backup_codes text NULL,
+	CONSTRAINT "PK_9ad9146e5846f04d44eb40299e8" PRIMARY KEY (id)
 );
-
-
--- public.caomei_auth_session definition
-
--- Drop table
-
--- DROP TABLE public.caomei_auth_session;
-
-CREATE TABLE public.caomei_auth_session (
-	id varchar(36) NOT NULL,
-	created_at timestamptz DEFAULT now() NOT NULL,
-	updated_at timestamptz DEFAULT now() NOT NULL,
-	expires_at timestamptz NOT NULL,
-	"token" text NOT NULL,
-	ip_address text NULL,
-	user_agent text NULL,
-	user_id varchar(36) NOT NULL,
-	impersonated_by varchar(36) NULL,
-	CONSTRAINT "PK_0a0af6fb921a4c466bc86ccdbe5" PRIMARY KEY (id)
-);
+CREATE INDEX "IDX_ed7df12901fcf60dc40654dc47" ON public.caomei_auth_two_factor USING btree (user_id);
 
 
 -- public.caomei_auth_user definition
@@ -65,8 +39,12 @@ CREATE TABLE public.caomei_auth_user (
 	banned bool DEFAULT false NOT NULL,
 	ban_reason text NULL,
 	ban_expires int4 NULL,
+	two_factor_enabled bool DEFAULT false NOT NULL,
 	CONSTRAINT "PK_8e449c5c3ca6bfe96e55e1891d0" PRIMARY KEY (id)
 );
+CREATE UNIQUE INDEX "IDX_7de537c03bf5bbe41219f4f2e2" ON public.caomei_auth_user USING btree (phone_number);
+CREATE UNIQUE INDEX "IDX_eaed40cc28f3c6cdcfdfbfea9c" ON public.caomei_auth_user USING btree (email);
+CREATE UNIQUE INDEX "IDX_fd9926712ead987f3ccb24744c" ON public.caomei_auth_user USING btree (username);
 
 
 -- public.caomei_auth_verification definition
@@ -84,3 +62,144 @@ CREATE TABLE public.caomei_auth_verification (
 	expires_at timestamptz NOT NULL,
 	CONSTRAINT "PK_c0451931f08cae54f9e77a828ed" PRIMARY KEY (id)
 );
+CREATE INDEX "IDX_21fa41ec0f7ea099140837714d" ON public.caomei_auth_verification USING btree (identifier);
+CREATE INDEX "IDX_fa68522a992ad7d16ac6f652d2" ON public.caomei_auth_verification USING btree (identifier, value);
+
+
+-- public.caomei_auth_account definition
+
+-- Drop table
+
+-- DROP TABLE public.caomei_auth_account;
+
+CREATE TABLE public.caomei_auth_account (
+	id varchar(36) NOT NULL,
+	created_at timestamptz DEFAULT now() NOT NULL,
+	updated_at timestamptz DEFAULT now() NOT NULL,
+	account_id text NOT NULL,
+	provider_id text NOT NULL,
+	user_id varchar(36) NOT NULL,
+	access_token text NULL,
+	refresh_token text NULL,
+	id_token text NULL,
+	access_token_expires_at timestamptz NULL,
+	refresh_token_expires_at timestamptz NULL,
+	"scope" text NULL,
+	"password" text NULL,
+	CONSTRAINT "PK_60bb887dbada52e154a3169c36e" PRIMARY KEY (id),
+	CONSTRAINT "FK_6415e014be607f933d913962c95" FOREIGN KEY (user_id) REFERENCES public.caomei_auth_user(id) ON DELETE CASCADE
+);
+CREATE INDEX "IDX_6415e014be607f933d913962c9" ON public.caomei_auth_account USING btree (user_id);
+
+
+-- public.caomei_auth_oauth_application definition
+
+-- Drop table
+
+-- DROP TABLE public.caomei_auth_oauth_application;
+
+CREATE TABLE public.caomei_auth_oauth_application (
+	id varchar(36) NOT NULL,
+	created_at timestamptz DEFAULT now() NOT NULL,
+	updated_at timestamptz DEFAULT now() NOT NULL,
+	client_id text NOT NULL,
+	client_secret text NOT NULL,
+	"name" text NOT NULL,
+	redirect_ur_ls text NOT NULL,
+	metadata text NULL,
+	"type" varchar(32) NOT NULL,
+	disabled bool DEFAULT false NOT NULL,
+	user_id varchar(36) NULL,
+	description text NULL,
+	token_endpoint_auth_method varchar(32) DEFAULT 'client_secret_basic'::character varying NULL,
+	grant_types text NULL,
+	response_types text NULL,
+	client_uri text NULL,
+	logo_uri text NULL,
+	"scope" text NULL,
+	contacts text NULL,
+	tos_uri text NULL,
+	policy_uri text NULL,
+	jwks_uri text NULL,
+	jwks text NULL,
+	software_id varchar(64) NULL,
+	software_version varchar(32) NULL,
+	software_statement text NULL,
+	CONSTRAINT "PK_c3d0590a580a761b8e4da8dd12c" PRIMARY KEY (id),
+	CONSTRAINT "FK_e99bf1ea40294f87fef9d3e13b6" FOREIGN KEY (user_id) REFERENCES public.caomei_auth_user(id) ON DELETE CASCADE
+);
+CREATE UNIQUE INDEX "IDX_dee3adb9afb19035fe52c3fd32" ON public.caomei_auth_oauth_application USING btree (client_id);
+CREATE INDEX "IDX_e99bf1ea40294f87fef9d3e13b" ON public.caomei_auth_oauth_application USING btree (user_id);
+
+
+-- public.caomei_auth_oauth_consent definition
+
+-- Drop table
+
+-- DROP TABLE public.caomei_auth_oauth_consent;
+
+CREATE TABLE public.caomei_auth_oauth_consent (
+	id varchar(36) NOT NULL,
+	created_at timestamptz DEFAULT now() NOT NULL,
+	updated_at timestamptz DEFAULT now() NOT NULL,
+	user_id varchar(36) NOT NULL,
+	client_id varchar(36) NOT NULL,
+	scopes text NULL,
+	consent_given bool DEFAULT false NOT NULL,
+	CONSTRAINT "PK_84bb78ae62c5414b679728f5000" PRIMARY KEY (id),
+	CONSTRAINT "FK_5537b38a6c06bb652a341fa6370" FOREIGN KEY (user_id) REFERENCES public.caomei_auth_user(id) ON DELETE CASCADE,
+	CONSTRAINT "FK_8a0738a41dabfbf705c162bde1d" FOREIGN KEY (client_id) REFERENCES public.caomei_auth_oauth_application(id) ON DELETE CASCADE
+);
+CREATE INDEX "IDX_5537b38a6c06bb652a341fa637" ON public.caomei_auth_oauth_consent USING btree (user_id);
+CREATE INDEX "IDX_8a0738a41dabfbf705c162bde1" ON public.caomei_auth_oauth_consent USING btree (client_id);
+
+
+-- public.caomei_auth_session definition
+
+-- Drop table
+
+-- DROP TABLE public.caomei_auth_session;
+
+CREATE TABLE public.caomei_auth_session (
+	id varchar(36) NOT NULL,
+	created_at timestamptz DEFAULT now() NOT NULL,
+	updated_at timestamptz DEFAULT now() NOT NULL,
+	expires_at timestamptz NOT NULL,
+	"token" text NOT NULL,
+	ip_address text NULL,
+	user_agent text NULL,
+	user_id varchar(36) NOT NULL,
+	impersonated_by varchar(36) NULL,
+	CONSTRAINT "PK_0a0af6fb921a4c466bc86ccdbe5" PRIMARY KEY (id),
+	CONSTRAINT "FK_74025fdcaa5eb6e1da65ecd0bcb" FOREIGN KEY (user_id) REFERENCES public.caomei_auth_user(id) ON DELETE CASCADE,
+	CONSTRAINT "FK_e01a03fbaf990eb8d81192ca5b2" FOREIGN KEY (impersonated_by) REFERENCES public.caomei_auth_user(id) ON DELETE SET NULL
+);
+CREATE INDEX "IDX_74025fdcaa5eb6e1da65ecd0bc" ON public.caomei_auth_session USING btree (user_id);
+CREATE INDEX "IDX_d18c48fcaf48365c685ebba665" ON public.caomei_auth_session USING btree (token);
+
+
+-- public.caomei_auth_oauth_access_token definition
+
+-- Drop table
+
+-- DROP TABLE public.caomei_auth_oauth_access_token;
+
+CREATE TABLE public.caomei_auth_oauth_access_token (
+	id varchar(36) NOT NULL,
+	created_at timestamptz DEFAULT now() NOT NULL,
+	updated_at timestamptz DEFAULT now() NOT NULL,
+	access_token text NOT NULL,
+	refresh_token text NULL,
+	access_token_expires_at timestamptz NOT NULL,
+	refresh_token_expires_at timestamptz NULL,
+	client_id varchar(36) NOT NULL,
+	user_id varchar(36) NOT NULL,
+	scopes text NULL,
+	CONSTRAINT "PK_9f0c527917a0691fa29d66dd185" PRIMARY KEY (id),
+	CONSTRAINT "FK_79dc2fd873300c86e710e1e5734" FOREIGN KEY (client_id) REFERENCES public.caomei_auth_oauth_application(id) ON DELETE CASCADE,
+	CONSTRAINT "FK_993218a00f98d6c01350c75c48a" FOREIGN KEY (user_id) REFERENCES public.caomei_auth_user(id) ON DELETE CASCADE
+);
+CREATE INDEX "IDX_64151152d3b06145934b2776db" ON public.caomei_auth_oauth_access_token USING btree (access_token);
+CREATE INDEX "IDX_79dc2fd873300c86e710e1e573" ON public.caomei_auth_oauth_access_token USING btree (client_id);
+CREATE INDEX "IDX_993218a00f98d6c01350c75c48" ON public.caomei_auth_oauth_access_token USING btree (user_id);
+CREATE INDEX "IDX_de6b575c18d7b60ef79b62c9f9" ON public.caomei_auth_oauth_access_token USING btree (refresh_token);
--- database/sqlite/create.sql
+++ database/sqlite/create.sql
@@ -1,11 +1,13 @@
 --
+-- SQLiteStudio v3.4.12 生成的文件，周五 7月 25 22:40:28 2025
+--
 -- 所用的文本编码：UTF-8
 --
 PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
 -- 表：caomei_auth_account
-DROP TABLE IF EXISTS caomei_auth_account;
+-- DROP TABLE IF EXISTS caomei_auth_account;
 
 CREATE TABLE IF NOT EXISTS caomei_auth_account (
     id                       VARCHAR (36) PRIMARY KEY
@@ -23,12 +25,117 @@ CREATE TABLE IF NOT EXISTS caomei_auth_account (
     access_token_expires_at  DATETIME,
     refresh_token_expires_at DATETIME,
     scope                    TEXT,
-    password                 TEXT
+    password                 TEXT,
+    CONSTRAINT FK_6415e014be607f933d913962c95 FOREIGN KEY (
+        user_id
+    )
+    REFERENCES caomei_auth_user (id) ON DELETE CASCADE
+                                     ON UPDATE NO ACTION
+);
+
+
+-- 表：caomei_auth_oauth_access_token
+-- DROP TABLE IF EXISTS caomei_auth_oauth_access_token;
+
+CREATE TABLE IF NOT EXISTS caomei_auth_oauth_access_token (
+    id                       VARCHAR (36) PRIMARY KEY
+                                          NOT NULL,
+    created_at               DATETIME     NOT NULL
+                                          DEFAULT (datetime('now') ),
+    updated_at               DATETIME     NOT NULL
+                                          DEFAULT (datetime('now') ),
+    access_token             TEXT         NOT NULL,
+    refresh_token            TEXT,
+    access_token_expires_at  DATETIME     NOT NULL,
+    refresh_token_expires_at DATETIME,
+    client_id                VARCHAR (36) NOT NULL,
+    user_id                  VARCHAR (36) NOT NULL,
+    scopes                   TEXT,
+    CONSTRAINT FK_79dc2fd873300c86e710e1e5734 FOREIGN KEY (
+        client_id
+    )
+    REFERENCES caomei_auth_oauth_application (id) ON DELETE CASCADE
+                                                  ON UPDATE NO ACTION,
+    CONSTRAINT FK_993218a00f98d6c01350c75c48a FOREIGN KEY (
+        user_id
+    )
+    REFERENCES caomei_auth_user (id) ON DELETE CASCADE
+                                     ON UPDATE NO ACTION
+);
+
+
+-- 表：caomei_auth_oauth_application
+-- DROP TABLE IF EXISTS caomei_auth_oauth_application;
+
+CREATE TABLE IF NOT EXISTS caomei_auth_oauth_application (
+    id                         VARCHAR (36) PRIMARY KEY
+                                            NOT NULL,
+    created_at                 DATETIME     NOT NULL
+                                            DEFAULT (datetime('now') ),
+    updated_at                 DATETIME     NOT NULL
+                                            DEFAULT (datetime('now') ),
+    client_id                  TEXT         NOT NULL,
+    client_secret              TEXT         NOT NULL,
+    name                       TEXT         NOT NULL,
+    redirect_ur_ls             TEXT         NOT NULL,
+    metadata                   TEXT,
+    type                       VARCHAR (32) NOT NULL,
+    disabled                   BOOLEAN      NOT NULL
+                                            DEFAULT (0),
+    user_id                    VARCHAR (36),
+    token_endpoint_auth_method VARCHAR (32) DEFAULT ('client_secret_basic'),
+    grant_types                TEXT,
+    response_types             TEXT,
+    client_uri                 TEXT,
+    logo_uri                   TEXT,
+    scope                      TEXT,
+    contacts                   TEXT,
+    tos_uri                    TEXT,
+    policy_uri                 TEXT,
+    jwks_uri                   TEXT,
+    jwks                       TEXT,
+    software_id                VARCHAR (64),
+    software_version           VARCHAR (32),
+    software_statement         TEXT,
+    description                TEXT,
+    CONSTRAINT FK_e99bf1ea40294f87fef9d3e13b6 FOREIGN KEY (
+        user_id
+    )
+    REFERENCES caomei_auth_user (id) ON DELETE CASCADE
+                                     ON UPDATE NO ACTION
+);
+
+
+-- 表：caomei_auth_oauth_consent
+-- DROP TABLE IF EXISTS caomei_auth_oauth_consent;
+
+CREATE TABLE IF NOT EXISTS caomei_auth_oauth_consent (
+    id            VARCHAR (36) PRIMARY KEY
+                               NOT NULL,
+    created_at    DATETIME     NOT NULL
+                               DEFAULT (datetime('now') ),
+    updated_at    DATETIME     NOT NULL
+                               DEFAULT (datetime('now') ),
+    user_id       VARCHAR (36) NOT NULL,
+    client_id     VARCHAR (36) NOT NULL,
+    scopes        TEXT,
+    consent_given BOOLEAN      NOT NULL
+                               DEFAULT (0),
+    CONSTRAINT FK_8a0738a41dabfbf705c162bde1d FOREIGN KEY (
+        client_id
+    )
+    REFERENCES caomei_auth_oauth_application (id) ON DELETE CASCADE
+                                                  ON UPDATE NO ACTION,
+    CONSTRAINT FK_5537b38a6c06bb652a341fa6370 FOREIGN KEY (
+        user_id
+    )
+    REFERENCES caomei_auth_user (id) ON DELETE CASCADE
+                                     ON UPDATE NO ACTION
 );
 
 
 -- 表：caomei_auth_session
-DROP TABLE IF EXISTS caomei_auth_session;
+-- DROP TABLE IF EXISTS caomei_auth_session;
 
 CREATE TABLE IF NOT EXISTS caomei_auth_session (
     id              VARCHAR (36) PRIMARY KEY
@@ -42,12 +149,46 @@ CREATE TABLE IF NOT EXISTS caomei_auth_session (
     ip_address      TEXT,
     user_agent      TEXT,
     user_id         VARCHAR (36) NOT NULL,
-    impersonated_by VARCHAR (36)
+    impersonated_by VARCHAR (36),
+    CONSTRAINT FK_74025fdcaa5eb6e1da65ecd0bcb FOREIGN KEY (
+        user_id
+    )
+    REFERENCES caomei_auth_user (id) ON DELETE CASCADE
+                                     ON UPDATE NO ACTION,
+    CONSTRAINT FK_e01a03fbaf990eb8d81192ca5b2 FOREIGN KEY (
+        impersonated_by
+    )
+    REFERENCES caomei_auth_user (id) ON DELETE SET NULL
+                                     ON UPDATE NO ACTION
+);
+
+
+-- 表：caomei_auth_two_factor
+-- DROP TABLE IF EXISTS caomei_auth_two_factor;
+
+CREATE TABLE IF NOT EXISTS caomei_auth_two_factor (
+    id           VARCHAR (36) PRIMARY KEY
+                              NOT NULL,
+    created_at   DATETIME     NOT NULL
+                              DEFAULT (datetime('now') ),
+    updated_at   DATETIME     NOT NULL
+                              DEFAULT (datetime('now') ),
+    user_id      VARCHAR (36) NOT NULL,
+    secret       TEXT         NOT NULL,
+    backup_codes TEXT,
+    CONSTRAINT UQ_4916ed9f34f6e9e0a0cfb89a752 UNIQUE (
+        user_id
+    ),
+    CONSTRAINT FK_ed7df12901fcf60dc40654dc47a FOREIGN KEY (
+        user_id
+    )
+    REFERENCES caomei_auth_user (id) ON DELETE CASCADE
+                                     ON UPDATE NO ACTION
 );
 
 
 -- 表：caomei_auth_user
-DROP TABLE IF EXISTS caomei_auth_user;
+-- DROP TABLE IF EXISTS caomei_auth_user;
 
 CREATE TABLE IF NOT EXISTS caomei_auth_user (
     id                    VARCHAR (36)  PRIMARY KEY
@@ -72,12 +213,14 @@ CREATE TABLE IF NOT EXISTS caomei_auth_user (
     banned                BOOLEAN       NOT NULL
                                         DEFAULT (0),
     ban_reason            TEXT,
-    ban_expires           INTEGER
+    ban_expires           INTEGER,
+    two_factor_enabled    BOOLEAN       NOT NULL
+                                        DEFAULT (0)
 );
 
 
 -- 表：caomei_auth_verification
-DROP TABLE IF EXISTS caomei_auth_verification;
+-- DROP TABLE IF EXISTS caomei_auth_verification;
 
 CREATE TABLE IF NOT EXISTS caomei_auth_verification (
     id         VARCHAR (36) PRIMARY KEY
@@ -89,6 +232,86 @@ CREATE TABLE IF NOT EXISTS caomei_auth_verification (
     identifier TEXT         NOT NULL,
     value      TEXT         NOT NULL,
     expires_at DATETIME     NOT NULL
+);
+
+
+-- 索引：IDX_5537b38a6c06bb652a341fa637
+DROP INDEX IF EXISTS IDX_5537b38a6c06bb652a341fa637;
+
+CREATE INDEX IF NOT EXISTS IDX_5537b38a6c06bb652a341fa637 ON caomei_auth_oauth_consent (
+    "user_id"
+);
+
+
+-- 索引：IDX_64151152d3b06145934b2776db
+DROP INDEX IF EXISTS IDX_64151152d3b06145934b2776db;
+
+CREATE INDEX IF NOT EXISTS IDX_64151152d3b06145934b2776db ON caomei_auth_oauth_access_token (
+    "access_token"
+);
+
+
+-- 索引：IDX_6415e014be607f933d913962c9
+DROP INDEX IF EXISTS IDX_6415e014be607f933d913962c9;
+
+CREATE INDEX IF NOT EXISTS IDX_6415e014be607f933d913962c9 ON caomei_auth_account (
+    "user_id"
+);
+
+
+-- 索引：IDX_74025fdcaa5eb6e1da65ecd0bc
+DROP INDEX IF EXISTS IDX_74025fdcaa5eb6e1da65ecd0bc;
+
+CREATE INDEX IF NOT EXISTS IDX_74025fdcaa5eb6e1da65ecd0bc ON caomei_auth_session (
+    "user_id"
+);
+
+
+-- 索引：IDX_79dc2fd873300c86e710e1e573
+DROP INDEX IF EXISTS IDX_79dc2fd873300c86e710e1e573;
+
+CREATE INDEX IF NOT EXISTS IDX_79dc2fd873300c86e710e1e573 ON caomei_auth_oauth_access_token (
+    "client_id"
+);
+
+
+-- 索引：IDX_8a0738a41dabfbf705c162bde1
+DROP INDEX IF EXISTS IDX_8a0738a41dabfbf705c162bde1;
+
+CREATE INDEX IF NOT EXISTS IDX_8a0738a41dabfbf705c162bde1 ON caomei_auth_oauth_consent (
+    "client_id"
+);
+
+
+-- 索引：IDX_993218a00f98d6c01350c75c48
+DROP INDEX IF EXISTS IDX_993218a00f98d6c01350c75c48;
+
+CREATE INDEX IF NOT EXISTS IDX_993218a00f98d6c01350c75c48 ON caomei_auth_oauth_access_token (
+    "user_id"
+);
+
+
+-- 索引：IDX_d18c48fcaf48365c685ebba665
+DROP INDEX IF EXISTS IDX_d18c48fcaf48365c685ebba665;
+
+CREATE INDEX IF NOT EXISTS IDX_d18c48fcaf48365c685ebba665 ON caomei_auth_session (
+    "token"
+);
+
+
+-- 索引：IDX_de6b575c18d7b60ef79b62c9f9
+DROP INDEX IF EXISTS IDX_de6b575c18d7b60ef79b62c9f9;
+
+CREATE INDEX IF NOT EXISTS IDX_de6b575c18d7b60ef79b62c9f9 ON caomei_auth_oauth_access_token (
+    "refresh_token"
+);
+
+
+-- 索引：IDX_ed7df12901fcf60dc40654dc47
+DROP INDEX IF EXISTS IDX_ed7df12901fcf60dc40654dc47;
+
+CREATE INDEX IF NOT EXISTS IDX_ed7df12901fcf60dc40654dc47 ON caomei_auth_two_factor (
+    "user_id"
 );
 
 
