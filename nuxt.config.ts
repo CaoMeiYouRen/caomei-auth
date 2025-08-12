6993c72a0c09ce02047160bc7a410078005522c6
CaoMeiYouRen <caomeiyouren@qq.com>
2025-08-12 10:36:34 +0800

    chore: 移除不必要的包 'winston' 和 'winston-daily-rotate-file' 的转译

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,7 +49,7 @@ export default defineNuxtConfig({
     ],
     build: {
         // 使用 Babel 转译不兼容的包
-        transpile: ['sqlite3', (ctx) => !ctx.isDev && 'google-libphonenumber', 'winston', 'winston-daily-rotate-file', 'async', 'logform', '@colors/colors'],
+        transpile: ['sqlite3', (ctx) => !ctx.isDev && 'google-libphonenumber', 'async', 'logform', '@colors/colors'],
     },
     css: [
         'normalize.css/normalize.css',
