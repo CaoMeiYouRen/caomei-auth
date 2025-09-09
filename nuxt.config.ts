1d5c164b7f9532ade84dcfce29f97522bad2d91b
CaoMeiYouRen <caomeiyouren@qq.com>
2025-09-10 02:17:48 +0800

    fix: 更新 nuxt.config.ts 配置文件

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -50,7 +50,7 @@ export default defineNuxtConfig({
     ],
     build: {
         // 使用 Babel 转译不兼容的包
-        transpile: ['sqlite3', (ctx) => !ctx.isDev && 'google-libphonenumber', 'async', 'logform', '@colors/colors'],
+        transpile: ['sqlite3', (ctx) => !ctx.isDev && 'google-libphonenumber'],
     },
     eslint: {
         config: {
