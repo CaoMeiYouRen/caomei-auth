22c4d18f410433d69c1814c20a57b14f49841e22
CaoMeiYouRen <caomeiyouren@qq.com>
2025-12-07 02:13:33 +0800

    feat: 添加 form-group 组件，封装 Label 和 Error Message 布局，替代 BaseSelect

---
+++ components/base/form-group.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="form-group">
+        <label
+            v-if="label"
+            :for="id"
+            class="form-label"
+        >
+            {{ label }}
+            <span v-if="required" class="required-mark">*</span>
+        </label>
+        <div class="form-control-wrapper">
+            <slot />
+        </div>
+        <div v-if="error" class="error-message">
+            {{ error }}
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    id?: string
+    label?: string
+    error?: string
+    required?: boolean
+}>()
+</script>
+
+<style lang="scss" scoped>
+.required-mark {
+    color: #e63946;
+    margin-left: 0.25rem;
+}
+
+.error-message {
+    color: #e63946;
+    font-size: 0.875rem;
+    margin-top: 0.25rem;
+}
+</style>
